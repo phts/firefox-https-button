a0f4fe1683739db810bc12746fb7418c437ef8b3
Phil Tsaryk <phil.tsarik@gmail.com>
2018-10-10 14:00:35 +0200

    Add toggle functionaly for https->http

--- src/lib/main.js
+++ src/lib/main.js
@@ -1,16 +1,22 @@
-async function switchToHttps() {
+function setTabUrl(tabId, url) {
+  browser.tabs.update(tabId, {
+    url,
+  })
+}
+
+async function toggleHttps() {
   const tabs = await browser.tabs.query({currentWindow: true, active: true})
   const currentTab = tabs[0]
   const tabId = currentTab.id
   const url = currentTab.url
-  if (!url.includes('http://')) {
-    return
-  }
 
-  const newUrl = url.replace('http://', 'https://')
-  browser.tabs.update(tabId, {
-    url: newUrl,
-  })
+  if (url.startsWith('http://')) {
+    const newUrl = url.replace('http://', 'https://')
+    setTabUrl(tabId, newUrl)
+  } else if (url.startsWith('https://')) {
+    const newUrl = url.replace('https://', 'http://')
+    setTabUrl(tabId, newUrl)
+  }
 }
 
-browser.browserAction.onClicked.addListener(switchToHttps)
+browser.browserAction.onClicked.addListener(toggleHttps)
